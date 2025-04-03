f9f2a57749b773949bd4feba3beb28c1bdd41b9a
lstransky <104894882+lstranskyTGM@users.noreply.github.com>
2025-04-03 09:30:32 +0200

    Update main.cpp pins for leds

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include "GPIO.hpp"
 
 // Define GPIO pins for each light
-#define RED_PIN    5
-#define YELLOW_PIN 6
-#define GREEN_PIN  7
+#define RED_PIN    2
+#define YELLOW_PIN 3
+#define GREEN_PIN  4
 
 // Define state timing values in ms
 #define RED_DURATION          4000
